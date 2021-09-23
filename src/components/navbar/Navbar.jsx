7ecb01e5e49d78e90c7e94878c61aaefabca6c27
kawsarm104 <kawsarm104@gmail.com>
2021-09-23 11:29:58 +0600

    header and navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './navbar.css'
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <nav>
+                <a href="">Shop Order</a>
+                <a href="">Review</a>
+                <a href="">Inventory Management</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
