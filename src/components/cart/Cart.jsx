a24c4bb3e8acb14204a047ee596ad6cb6fd64bd9
kawsarm104 <kawsarm104@gmail.com>
2021-09-24 16:11:47 +0600

    implement search feature

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -3,15 +3,22 @@ import './Cart.css'
 
 const Cart = (props) => {
     // console.log(props.cart)
+    // console.log(props.cart)
     const { cart } = props
+    let totalQuantity = 0
     let total = 0;
     for (const product of cart) {
-        total = total+product.price
+        if (!product.quantity){
+            product.quantity = 1;
+        }
+        console.log("carrt",product)
+        total = total + (product.price*product.quantity)
+        totalQuantity = totalQuantity+product.quantity
     }
     return (
         <div>
                <h2>Order Summary</h2>
-            <h3>Items Ordered: {props.cart.length}</h3>
+            <h3>Items Ordered: {totalQuantity}</h3>
             <h4>Total Price: ${total.toFixed(2)}</h4>
         </div>
     );
--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { addToDb } from '../../utilities/fakedb';
+import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../cart/Cart';
 // import Product from './Product';
 import Product from './Product';
@@ -7,25 +7,74 @@ import './shop.css';
 const Shop = () => {
     const [products, setProducts] = useState([])
     const [cart, setCart] = useState([])
+    const [displayProduct, setDisplayProduct] = useState([])
     
     useEffect(() => {
+        console.log('product api called ')
         fetch('./products.json')
             .then(res => res.json())
-        .then(data => setProducts(data))
+            .then(data => {
+                setProducts(data)
+                setDisplayProduct(data)
+                console.log('product received')
+            })
     }, [])
+    // getting local storage data 
+    useEffect(() => {
+        console.log("local storage called ")
+        
+        // console.log(savedCart)
+       if(products.length){
+           const savedCart = getStoredCart()
+           const storedCart = []
+           for (const key in savedCart) {
+            //    console.log(key, savedCart[key])
+               
+            // console.log(key)
+            // console.log(products)
+            const addedProduct = products.find(product => product.key === key)
+                //   console.log(key, addedProduct)
+               if (addedProduct) {
+                   const quantity = savedCart[key]
+                   addedProduct.quantity = quantity;
+                //    console.log(addedProduct)
+                   storedCart.push(addedProduct)
+               }
+                  
+           }
+           setCart(storedCart)
+      }
+    }, [products]) // dependency 
     const handleAddToCart = product => {
-        console.log(product.name)
+        // console.log(product.name)
         const newCart = [...cart, product]
         // console.log(newCart)
         setCart(newCart)
         // Save to local storage for now 
         addToDb(product.key)
     }
+    // hendling search 
+    const handleSearch = event => {
+        const searchText = event.target.value;
+        const matchedProducts = products.filter(product => product.name.toLowerCase().includes(searchText.toLowerCase()))
+        // console.log(matchedProducts.length)
+        setDisplayProduct(matchedProducts)
+
+    }
+
     return (
+        <>
+            <div className="search-container">
+                <input type="text"
+                    onChange={handleSearch}
+                placeholder="Search Your Product"/>
+            </div>
+            
+            
         <div className="shop-container">
             <div className="product-container">
                 {/* <h3>Products: {products.length} </h3> */}
-                {products.map(product => <Product
+                {displayProduct.map(product => <Product
                     key={product.key}
                     product={product}
                     handleAddToCart = {handleAddToCart}
@@ -34,7 +83,8 @@ const Shop = () => {
             <div className="cart-container">
                 <Cart cart={cart}/>
             </div>
-        </div>
+            </div>
+            </>
     );
 };
 
